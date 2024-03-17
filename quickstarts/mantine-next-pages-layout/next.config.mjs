5c4cfb9fd11ae0185267af53f2fcfb231d27067d
iAgonyii <mail@agony.dev>
2024-03-17 04:20:05 +0100

    update mantine quickstart

--- quickstarts/mantine-next-pages-layout/next.config.mjs
+++ quickstarts/mantine-next-pages-layout/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
+    reactStrictMode: false,
+    swcMinify: true,
+    webpack: (config) => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding');
+        return config;
+    },
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
 };
 
 export default nextConfig;
